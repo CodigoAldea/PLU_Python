8f78ab3bc960d93680dc36628ab30b5d9334ef2d
pujakumari12322367 <pk3056630@gmail.com>
2026-06-29 12:16:54 +0530

    First commit

---
+++ Section B.py/1st Question.py
@@ -0,0 +1,6 @@
+a = int(input("Enter an integer: "))
+b = float(input("Enter a float: "))
+c = input("Enter a string: ")
+print(type(a))
+print(type(b))
+print(type(c))
---
+++ Section B.py/6th Question.py
@@ -0,0 +1,3 @@
+import math
+print("Square Root of 64 =", math.sqrt(64))
+print("Value of Pi =", math.pi)
