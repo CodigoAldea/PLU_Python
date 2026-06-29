7ab9c7bc51cb6f1f721e878a08e8d79174b9d1cc
Amit kumar <amitakhil001@gmail.com>
2026-06-29 12:05:10 +0530

    Refactor multiplication table script to use a reusable function and main guard

--- Test1/sectionB_04.py
+++ Test1/sectionB_04.py
@@ -1,11 +1,11 @@
 '''4. Print the multiplication table of a given number up to 10.'''
 # n = int(input("Enter number: "))
 # for i in range(1, 11):
-#     print(n, "a", i, ":", n * i)
+#     print(n, "X", i, ":", n * i)
 
     
 def table():
     n = int(input("Enter number: "))
     for i in range(1, 11):
-        print(n, "a", i, ":", n * i)
+        print(n, "X", i, ":", n * i)
 table()
